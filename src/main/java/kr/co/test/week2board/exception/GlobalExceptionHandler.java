6ad772690d67cba28e7a5c7f3242cf031d40539e
itsme <soi8511240@gmail.com>
2025-03-21 15:41:57 +0900

    공통 응답객체 추가

--- src/main/java/kr/co/test/week2board/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/co/test/week2board/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.co.test.week2board.exception;
 
+import kr.co.test.week2board.board.util.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,21 +19,28 @@ import java.util.Map;
 //@Controller
 
 @RestControllerAdvice
-@Controller
 public class GlobalExceptionHandler {
+
+    // 에러 메시지 템플릿
+    protected static final Map<Class<? extends Exception>, String> ERROR_MESSAGES = Map.of(
+            NullPointerException.class, "필수 값이 누락되었습니다.",
+            IllegalArgumentException.class, "잘못된 인자가 제공되었습니다.",
+            IllegalStateException.class, "현재 애플리케이션 상태가 유효하지 않습니다.",
+            RuntimeException.class, "런타임 예외가 발생했습니다.",
+            IOException.class, "파일처리중 예외가 발생했습니다.",
+            Exception.class, "처리 중에 에러가 발생했습니다." // 기본 메시지
+    );
+
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<Map<String,String>> handleException(Exception e) {
-        HttpHeaders headers = new HttpHeaders();
-//        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        HttpStatus status = HttpStatus.BAD_REQUEST;
+    public CommonResponse<String> handleException(Exception e) {
+        String defaultMessage = ERROR_MESSAGES.getOrDefault(Exception.class, "알 수 없는 예외가 발생했습니다.");
+        String message = ERROR_MESSAGES.getOrDefault(e.getClass(), defaultMessage);
+        // 새로운 예외와 메시지 추가
+        // ERROR_MESSAGES.put(FileNotFoundException.class, "요청한 파일을 찾을 수 없습니다.");
+        // ERROR_MESSAGES.put(SQLException.class, "데이터베이스 작업 중 오류가 발생했습니다.");
 
-        Map<String,String> map = new HashMap<>();
-        map.put("error", status.getReasonPhrase());
-        map.put("code", e.getClass().getSimpleName()); // Todo : 정해진약이 있다면.. 줌
-        map.put("message", e.getMessage()); // Todo:  필요한부분만. 전체를 다주면안됨.
-
-        log.error("############################################:{}", e.getMessage());
-        return new ResponseEntity<>(map, headers, status);
+        log.error("\n\n\n\n\n\n############################################ errorHandler:{}\n\n\n\n\n\n\n\n", e.getMessage());
+        return CommonResponse.apiResponse(e, message);
     }
 
 }
